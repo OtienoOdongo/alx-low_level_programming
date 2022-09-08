ea2e987da0244614c99673056f7b63d14315c249
OtienoOdongo <otabiznake@gmail.com>
2022-09-08 11:49:36 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - prints
- *
- * Return: 0 
- */
+
+* main - Printing
+
+*
+
+* Return: Always 0 (Successful)
+
+*/
+
 int main(void)
+
 {
-   puts("\""Programming is like building a multilingual puzzle");
-   return (0);
+
+puts("\"Programming is like building a multilingual
+
+puzzle");
+
+return (0);
+
 }
