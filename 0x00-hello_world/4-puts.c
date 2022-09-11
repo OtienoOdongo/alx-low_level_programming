43976c0f2202c161c196c9a113ad194d36838ed6
OtienoOdongo <otabiznake@gmail.com>
2022-09-11 12:50:44 -0700

    it will work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - entry point
  *
  * Return: Always 0 (Success)
  */
@@ -9,5 +9,5 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	return (0);
 }
