87a6dc57d57366c0fae95e0be374493b69734cac
OtienoOdongo <otabiznake@gmail.com>
2022-09-29 09:00:59 -0700

    a function that prints a string, followed by a new line

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * void prototype
- * int prototype
- */
-
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -13,5 +6,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-
-#endif
+int is_palidrome(char *s);
